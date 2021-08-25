682a9bd0f007f8a51eb0dbb74e49feb2d2a79ea0
JamieDanielson <jamieedanielson@gmail.com>
2021-08-25 18:07:12 -0400

    WIP - Add Support for netcore2.1

---
+++ examples/netcore21console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenTelemetry.Trace;
+using Honeycomb.OpenTelemetry;
+
+namespace netcore21console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // configure HoneycombOptions
+            var options = new HoneycombOptions
+            {
+                ServiceName = "my-console-app",
+                ApiKey = "{apikey}",
+                Dataset = "jamie",
+            };
+
+            // configure OpenTelemetry SDK to send data to Honeycomb
+            // NOTE: the tracer provider should be a long-lived resource, and disposed
+            // at the end of your app lifecycle to ensure all telemetry is exported
+            using var provider = OpenTelemetry.Sdk.CreateTracerProviderBuilder()
+                .AddHoneycomb(options)
+                .Build();
+            
+            // get an instance of a tracer that can be used to create spans
+            var tracer = provider.GetTracer(options.ServiceName);
+
+            // create span to describe some application logic
+            using var span = tracer.StartActiveSpan("doSomething");
+            span.SetAttribute("user_id", 123);
+        }
+    }
+}
---
+++ examples/netcoreapp21/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netcoreapp21.Models;
+using OpenTelemetry.Trace;
+
+namespace netcoreapp21.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Tracer _tracer;
+        public HomeController(Tracer tracer)
+        {
+            _tracer = tracer;
+        }
+        public async Task<string> Tester()
+        {
+            using var span = _tracer.StartActiveSpan("sleep");
+            span.SetAttribute("delay_ms", 100);
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            return "test";
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
